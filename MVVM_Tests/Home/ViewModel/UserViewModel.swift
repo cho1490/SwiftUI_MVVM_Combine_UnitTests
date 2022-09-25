1bc087354d5c476f986fc5e4129a8f7ff562c3db
cho1490 <chosanghyun99@gmail.com>
2022-09-25 23:15:49 +0900

    해결git add .! (로딩뷰가 sink를 호출한 뷰를 가리면서 인스턴스가 삭제됨 -> AnyCancellable가 publisher 파괴함)

--- MVVM_Tests/Home/ViewModel/UserViewModel.swift
+++ MVVM_Tests/Home/ViewModel/UserViewModel.swift
@@ -11,32 +11,16 @@ class UserViewModel: BaseViewModel {
             
     let version = "v4"
     
+    private var cancellables2 = Set<AnyCancellable>()
+    
     @Published var userName: String = ""
     @Published var user: User?        
             
     func getData() {
-        loadingSingleton.loading()
+//        loadingSingleton.loading()
         
         let endPoint = "\(version)/summoners/by-name/\(userName)"
         NetworkManager.shared.getSingleData(startPoint: .kr, middlePoint: .summoner, endPoint: endPoint, type: User.self)
-            .handleEvents(receiveSubscription: { (subscription) in
-                print("Receive Subscription")
-            }, receiveOutput: { output in
-                print("Receive Output : \(output)")
-            }, receiveCompletion: { (completion) in
-                print("Receive Completion")
-                switch completion {
-                case .finished:
-                    print("finished")
-                case .failure(let error):
-                    print(error)
-                }
-            }, receiveCancel: {
-//                print("!!! \(self.user?.accountId ?? "ㅠㅠ")")
-                print("Receive Cancel")
-            }, receiveRequest: { demand in
-                print("Receive Request: \(demand)")
-            })
             .sink { [weak self] completion in
                 switch completion {
                 case .failure(let error):
@@ -44,16 +28,14 @@ class UserViewModel: BaseViewModel {
                 case .finished:
                     print("UserViewModel getData Finished")
                 }
-
-                print("호출 좀")
+                
                 self?.loadingSingleton.complete()
             } receiveValue: { [weak self] user in
                 self?.user = user
-
-                print("되라")
+                
                 self?.loadingSingleton.complete()
             }
-            .store(in: &cancellables)
+            .store(in: &cancellables2)
     }
     
 }
